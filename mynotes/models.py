1c1db64869ebea70e903438827af26b7770ee5e0
Daniel Lusdyk <dan.lusdyk@gmail.com>
2022-03-19 16:23:51 -0400

    Changed database timezone

--- mynotes/models.py
+++ mynotes/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.utils import timezone
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
@@ -8,7 +9,7 @@ from django.urls import reverse
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    lastUpdated = models.DateTimeField(auto_now=True)
+    lastUpdated = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -16,3 +17,7 @@ class Note(models.Model):
 
     def get_absolute_url(self):
         return reverse('note-detail', kwargs={'pk':self.pk})
+
+    def save(self, *args, **kwargs):
+        self.lastUpdated = timezone.now()
+        return super(Note, self).save(*args, **kwargs)
